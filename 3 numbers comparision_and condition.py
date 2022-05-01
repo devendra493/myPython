5ff688991aaf131c9a47f532ce862ce4c956b586
Devendra <devendr493@gmail.com>
2022-05-01 10:23:09 +0530

    my python programme 1st commit

---
+++ 3 numbers comparision_and condition.py
@@ -0,0 +1,10 @@
+a = 5
+b = 1
+c = 15
+
+if a < b and a < c:
+    print('a is the smallest with value=', a)
+elif b < c:
+    print('b is the smallest with value=', b)
+else:
+    print('c is the smallest with value=', c)
---
+++ comparision of 2 numbers.py
@@ -0,0 +1,7 @@
+a = 30
+b = 20
+
+if a < b:
+    print('a is the smallest with value=', a)
+else:
+    print('b is the smallest with value=', b)
